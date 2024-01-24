c07c4b81b7d250b8ee3219e7313fabe967f8af1f
MicaRios <camila.c.m.r@gmail.com>
2024-01-23 23:31:10 -0300

    Ejercicio 1
    Uso de 'let', 'prompt' y 'alert'

---
+++ app.js
@@ -0,0 +1,17 @@
+alert('¡Bienvenida y bienvenido a nuestro sitio web!')
+
+let nombre = 'Luna';
+let edad = 25;
+let numeroDeVentas = 50;
+let saldoDisponible = 1000;
+let mensajeDeError ='¡Error! Completa todos los campos';
+
+alert('¡Error! Completa todos los campos');
+alert(mensajeDeError);
+
+ nombre = prompt('¿Cuál es tu nombre de usuario');
+ edad = prompt('¿Cuál es tu edad?')
+
+if ( edad >= 18){
+    alert('¡Puedes obtener tu licencia de conducir')
+};
